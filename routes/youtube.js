eda635abcab8ab8bf7ac625b97eda81873bfaf2a
Aiden <amb@aspy.dev>
2022-04-08 16:51:02 -0400

    :bug: fix youtube failing to catch 404s properly

--- routes/youtube.js
+++ routes/youtube.js
@@ -15,16 +15,23 @@ export async function endpoint(fastify) {
                     data.views = json.items[0].statistics.viewCount;
                     data.videos = json.items[0].statistics.videoCount;
                     data.username = json.items[0].snippet.title;
-                    data.description = json.items[0].snippet.description;
-                    data.avatar = json.items[0].snippet.thumbnails.high.url;
+                    data.description = json.items[0]?.snippet.description;
+                    data.avatar = json.items[0]?.snippet.thumbnails.high.url;
                 });
             return {
                 code: 200,
                 data,
             };
         } catch (err) {
-            console.error(`Threw 500 error in YouTube module: ${err}`);
-            return {code: 500, message: err.message};
+            if (err.message == "Cannot read properties of undefined (reading '0')") {
+                return {
+                    code: 404,
+                    message: 'Channel by the given ID does not exist (The channel ID is not the channel name!)',
+                };
+            } else {
+                console.error(`Threw 500 error in YouTube module: ${err}`);
+                return {code: 500, message: err.message};
+            }
         }
     });
 }
